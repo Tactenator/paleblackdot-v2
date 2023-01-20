f03b1da8d303bb8b004eb1a34549b3eacfaa1cf3
Tactenator <trevor.mclaurine@gmail.com>
2023-01-19 21:22:12 -0600

    Added contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,47 @@
+import { useState } from "react"
+
 const Contact = () => {
+
+    const [ name, setName ] = useState()
+    const [ email, setEmail ] = useState()
+    const [ phone, setPhone ] = useState()
+    const [ message, setMessage ] = useState()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`Name: ${name} Email: ${email} Phone: ${phone} Message: ${message}`)
+        // maybe send a modal? Maybe change to another SVG for something received
+    }
     return ( 
     <div id="contact">
-        Contact
+        <h1>Let's Get Started</h1>
+        <form className="contact-form">
+            <label>Name</label>
+            <input type='text'
+            onChange={((e) => setName(e.target.value))}
+            value={name}
+            ></input>
+
+            <label>Email</label>
+            <input type='email'
+            onChange={((e) => setEmail(e.target.value))}
+            value={email}
+            ></input>
+
+            <label>Phone</label>
+            <input type='tel'
+            onChange={((e) => setPhone(e.target.value))}
+            value={phone}
+            ></input>
+
+            <label>Message</label>
+            <textarea rows={10} cols={10}
+            onChange={((e) => setMessage(e.target.value))}
+            value={message}
+            ></textarea>
+
+            <button onClick={((e) => handleSubmit(e))}>Submit</button>
+        </form>
     </div> );
 }
  
